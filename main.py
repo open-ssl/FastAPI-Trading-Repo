472efdf804b75a936c19e11984e8d2d59e1684df
Stanislav Lukyanov <>
2023-02-15 12:57:12 +0400

    add validation for data in request

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+
 from fastapi import FastAPI
+
+#  uvicorn main:app --reload
+from pydantic import BaseModel
 
 app = FastAPI(
     title="Trading App"
@@ -9,6 +16,9 @@ fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
     {"id": 2, "role": "investor", "name": "Max"},
     {"id": 3, "role": "traider", "name": "Stas"},
+    {"id": 4, "role": "investor", "name": "Stas", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]},
 ]
 
 fake_extra_users = [
@@ -23,17 +33,36 @@ fake_trades = [
 ]
 
 
-@app.get("/users/{user_id}")
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str
+    side: str
+    price: int
+    amount: float
+
+
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = []
+
+
+@app.get("/users/{user_id}", response_model=List[User])
 def get_data_for_user(user_id: int):
-    user = list(filter(lambda x: x.get("id") == int(user_id), fake_users))
-    if not user:
-        return {}
-    return user[0]
-
-
-@app.get("/trades")
-def get_trades(limit: int = 1, offset: int = 1):
-    return fake_trades[offset:][:limit]
+    return [user for user in fake_users if user.get("id") == int(user_id)]
 
 
 @app.post("/users/{user_id}")
@@ -45,3 +74,10 @@ def change_user_name(user_id: int, new_name: str):
     user = user[0]
     user["name"] = new_name
     return {"status": 200, "operation_result": user}
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
+
