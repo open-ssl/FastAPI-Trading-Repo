163ccd6b95c6fc080e120ff1502bd1e8cc8a0ef2
Stanislav Lukyanov <lukyanov.stas@mail.ru>
2023-07-27 18:19:34 +0200

    update extra fields for users model

--- models/models.py
+++ models/models.py
@@ -1,26 +1,29 @@
 from datetime import datetime
 
 from sqlalchemy import (
-    MetaData, Column, Table, Integer, String, TIMESTAMP, ForeignKey, JSON
+    MetaData, Column, Table, Integer, String, Boolean, TIMESTAMP, ForeignKey, JSON
 )
 
 metadata = MetaData()
 
-roles = Table(
-    "roles",
+role = Table(
+    "role",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
     Column("permissions", JSON),
 )
 
-users = Table(
-    "users",
+user = Table(
+    "user",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("name", String, nullable=False),
-    Column("password", String, nullable=False),
+    Column("hashed_password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.now),
-    Column("role_id", Integer, ForeignKey("roles.id")),
+    Column("role_id", Integer, ForeignKey(role.c.id)),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
 )
